a4509c2ce43e23368c8af1fca4c7e61b61518000
Hosurshameer <shameerhosur29@gmail.com>
2025-12-25 09:38:30 +0530

    Added Cart Functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,8 +3,15 @@ import { Link } from "react-router-dom";
 import emptyCartImage from "../assets/util/emptyCart.png";
 import PageTltle from "./PageTltle";
 import { useNavigate } from "react-router-dom";
+import { useCart } from "../store/cart-context";
+import { useMemo } from "react";
+import CartTable from "./CartTable";
 
 export default function Cart() {
+  const { cart } = useCart();
+
+  const isEmpty = useMemo(() => cart.length === 0, [cart.length]);
+
   const navigation = useNavigate();
   function handleClick() {
     navigation("/home", { state: { username: "sameer" } });
@@ -13,22 +20,41 @@ export default function Cart() {
     <div className="min-h-[852px] py-12 bg-normalbg dark:bg-darkbg font-primary">
       <div className="max-w-4xl mx-auto px-4">
         <PageTltle title="Your Cart" />
-        <div className="text-center text-gray-600 dark:text-lighter flex flex-col items-center">
-          <p className="max-w-[576px] px-2 mx-auto text-base mb-4">
-            Oops... Your cart is empty. Continue Shopping
-          </p>
-          <img
-            src={emptyCartImage}
-            alt="Empty Cart"
-            className="max-w-[300px] mx-auto mb-6 dark:bg-light dark:rounded-md"
-          />
-          <button
-            onClick={handleClick}
-            className="py-2 px-4 bg-primary dark:bg-light text-white dark:text-black text-xl font-semibold rounded-sm flex justify-center items-center hover:bg-dark dark:hover:bg-lighter transition"
-          >
-            Back to Products
-          </button>
-        </div>
+        {!isEmpty ? (
+          <>
+            <CartTable />
+            <div className="flex justify-between mt-8 space-x-4">
+              {/* Back to Products Button */}
+              <Link
+                to="/home"
+                className="py-2 px-4 bg-primary dark:bg-light text-white dark:text-black text-xl font-semibold rounded-sm flex justify-center items-center hover:bg-dark dark:hover:bg-lighter transition"
+              >
+                Back to Products
+              </Link>
+              {/* Proceed to Checkout Button */}
+              <button className="py-2 px-4 bg-primary dark:bg-light text-white dark:text-black text-xl font-semibold rounded-sm flex justify-center items-center hover:bg-dark dark:hover:bg-lighter transition">
+                Proceed to Checkout
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className="text-center text-gray-600 dark:text-lighter flex flex-col items-center">
+            <p className="max-w-[576px] px-2 mx-auto text-base mb-4">
+              Oops... Your cart is empty. Continue Shopping
+            </p>
+            <img
+              src={emptyCartImage}
+              alt="Empty Cart"
+              className="max-w-[300px] mx-auto mb-6 dark:bg-light dark:rounded-md"
+            />
+            <Link
+              to="/home"
+              className="py-2 px-4 bg-primary dark:bg-light text-white dark:text-black text-xl font-semibold rounded-sm flex justify-center items-center hover:bg-dark dark:hover:bg-lighter transition"
+            >
+              Back to Products
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
