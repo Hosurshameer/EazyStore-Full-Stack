90864fb6f2fc9f9da7fbdb38b6a96c7f7a8a121d
Hosurshameer <shameerhosur29@gmail.com>
2026-01-13 17:26:51 +0530

    ....

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,7 +28,6 @@ export async function productsLoader() {
     return response.data;
   } catch (error) {
     throw new Response(
-      console.log(error)
       error.response?.data?.errorMessage ||
         error.message ||
         "Failed to fetch the products  .Pleasse try again",
