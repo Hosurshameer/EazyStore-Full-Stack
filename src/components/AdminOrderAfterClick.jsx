6d8ed331393dc264f4a5f87767ac4e8a52b6c06e
Hosurshameer <shameerhosur29@gmail.com>
2026-02-18 10:46:48 +0530

    ......

--- src/components/AdminOrderAfterClick.jsx
+++ src/components/AdminOrderAfterClick.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
+import PageTltle from './PageTltle';
 
 export default function AdminOrderAfterClick() {
 
     const location=useLocation();
     const item=location.state;
   return (
-    <div>
+    <div className="w-64 mx-auto mt-40 bg-blue-200">
+
+       
         
          <img src={item.imageUrl}
          
          alt={item.name}
-         className="w-full h-full object-cover transition-transform duration-500 ease-in-out hover:scale-110"
+         className="w-small h-full object-cover transition-transform duration-500 ease-in-out hover:scale-110"
 
          />
        
