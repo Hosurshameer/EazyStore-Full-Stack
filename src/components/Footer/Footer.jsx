4dcaab87500c348eae021b8911d196c1f88b1510
Hosurshameer <shameerhosur29@gmail.com>
2026-02-18 10:51:28 +0530

    Changed the footer name

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,7 +17,7 @@ export default function Footer() {
         rel="noreferrer"
         className="text-primary dark:text-primary font-semibold px-1 transition-colors duration-300 hover:text-dark dark:hover:text-[#0080a3]"
       >
-        eazybytes
+        Sameer sandy
       </a>
     </footer>
   );
