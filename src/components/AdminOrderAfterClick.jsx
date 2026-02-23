197bad45b3d0c6f76067e1868f922a807be47df3
Hosurshameer <shameerhosur29@gmail.com>
2026-02-23 10:24:42 +0530

    Added the back to ordersButton in the AdminOrdersAfterClick component

--- src/components/AdminOrderAfterClick.jsx
+++ src/components/AdminOrderAfterClick.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import PageTltle from './PageTltle';
+import { Link } from 'react-router-dom';
 
 export default function AdminOrderAfterClick() {
 
@@ -16,11 +17,22 @@ export default function AdminOrderAfterClick() {
          <img src={item.imageUrl}
          
          alt={item.name}
-         className="w-small h-full object-cover transition-transform duration-500 ease-in-out hover:scale-110"
+         className=" h-full object-cover transition-transform duration-500 ease-in-out hover:scale-110"
 
          />
+
        
     </div>
+       <div className="w-64 mx-auto mt-10 bg-blue-200">
+           <Link
+              to="/admin/orders"
+              className=" mx-auto   py-2 px-4 bg-primary dark:bg-light text-white dark:text-black text-xl font-semibold rounded-sm flex justify-center items-center hover:bg-dark dark:hover:bg-lighter transition"
+            >
+              Back to Orders
+            </Link>
+            </div>
+    
+         
     </>
   )
 }
